05fdf988c747dc7b99be67904f04281c01a44507
Kaushal273 <kaushalyadav536@gmail.com>
2023-07-24 20:59:37 +0530

    fourth commit

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ require('./db/config');
 const User = require("./db/User");
 const Product = require("./db/Product");
 const app = express();
+const Jwt = require('jsonwebtoken');
+const jwtKey = 'e-comm';
 
 app.use(express.json());
 app.use(cors());
@@ -13,7 +15,12 @@ app.post("/register",async (req,res) =>{
     let result = await user.save();
     result = result.toObject();
     delete result.password;
-    res.send(result);
+    Jwt.sign({result},jwtKey,{expiresIn: '2h'}, (err,token) =>{
+        if(err){
+            res.send({result:"Something went wrong, please try after sometime"})
+        }
+        res.send({result,auth: token})
+    })
 })
 
 app.post("/login",async (req,res)=>{
@@ -22,7 +29,13 @@ app.post("/login",async (req,res)=>{
 
         let user = await User.findOne(req.body).select("-password");
         if(user){
-            res.send(user)
+            Jwt.sign({user},jwtKey,{expiresIn: '2h'}, (err,token) =>{
+                if(err){
+                    res.send({result:"Something went wrong, please try after sometime"})
+                }
+                res.send({user,auth: token})
+            })
+            
         }else{
             res.send({result:'No User Found'});
         }
@@ -38,6 +51,50 @@ app.post("/add-product", async (req,res) => {
     res.send(result);
 })
 
+app.get("/products",async (req,res) =>{
+    let products = await Product.find();
+    if(products.length>0){
+        res.send(products)
+    }else{
+        res.send({result: "No Products Found"})
+    }
+})
 
+app.delete("/product/:id", async (req,res)=>{
+    const result = await Product.deleteOne({_id:req.params.id});
+    res.send(result);
+})
+
+app.get("/product/:id", async (req,res)=>{
+    let result = await Product.findOne({_id:req.params.id});
+    if (result){
+        res.send(result)
+    }else{
+        res.send({result:"No Record Found"});
+    }
+});
+
+app.put("/product/:id", async (req,res)=>{
+    let result = await Product.updateOne(
+        {_id: req.params.id},
+        {
+            $set :req.body
+        }
+        )
+        res.send(result);
+    })
+
+app.get("/search/:key" , async (req,res) =>{
+    let result = await Product.find({
+        "$or": [
+            
+                {name:{ $regex:req.params.key}},
+                {company: {$regex: req.params.key}},
+                {category: {$regex: req.params.key}}
+            
+        ]
+    });
+    res.send(result);
+})
 
 app.listen(5000);
